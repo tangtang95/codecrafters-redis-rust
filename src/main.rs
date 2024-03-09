bcaf868b52e4b5e26665ad3c50550a24a1772f31
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-03-09 15:54:01 +0100

    chore: return PONG

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    println!("Redis listening on port 6379");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write_all("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
