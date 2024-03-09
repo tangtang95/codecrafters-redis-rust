70d2f135262f36b153487b8a83f5202efe04eee9
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-03-09 16:39:42 +0100

    chore: handle multiple redis ping commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,11 +13,23 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                _stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+                let _ = handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
+    let mut buf = [0u8; 256];
+
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write_all(b"+PONG\r\n")?;
     }
 }
